4c1a5e4ca2deadb518adbfea3b7e5e2a5629bf36
Jose Xavier Fuentes <josexfuentes27@gmail.com>
2024-04-14 15:25:35 -0700

    cleaned up match model

--- app/models/match.js
+++ app/models/match.js
@@ -9,46 +9,35 @@ module.exports = (sequelize, DataTypes) => {
             },
             fixture_id: {
                 type: DataTypes.STRING,
-                //allowNull: false,
             },
             home_team: {
                 type: DataTypes.STRING,
-                //allowNull: false,
             },
             away_team: {
                 type: DataTypes.STRING,
-                //allowNull: false,
             },
             league: {
                 type: DataTypes.STRING,
-                //allowNull: false,
             },
             stadium: {
                 type: DataTypes.STRING,
-                //allowNull: false,
             },
             home_goals: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                //allowNull: false,
             },
             away_goals: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                //allowNull: false,
             },
             first_scorer: {
                 type: DataTypes.STRING,
-                defaultValue: "No One",
-                //allowNull: false,
             },
             date: {
                 type: DataTypes.DATE,
-                //allowNull: false,
             },
             time: {
                 type: DataTypes.TIME, 
-                //allowNull: false,
             },
             finished: {
                 type: DataTypes.BOOLEAN,
