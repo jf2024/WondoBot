58375a01fc0af599a3d6882733faeabe942c4ae3
Jose Xavier Fuentes <josexfuentes27@gmail.com>
2024-04-15 01:00:01 -0700

    got predict command to work, some comments for future steps and comments for user model

--- app/commands/utility/predict.js
+++ app/commands/utility/predict.js
@@ -1,47 +1,97 @@
+/*
+- Necessary thing for this file
+a) When a user types in a scorer, need to check if the player is indeed part 
+of the SJ earthquakes. If it's not a valid name, tell user to try again.
+b) check grace period as described in the function near the bottom of the file
+
+Optional part for this file
+a) currently, picture when user makes a prediction is just the user's profile picture,
+maybe in the future, we can have a picture of the player that the user predicts to score first
+some code for this or a template is in the api.js file 
+*/
+
+
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const { getCurrentMatch } = require("../../api-functions/api");
+const { Prediction, User, Match } = require("../../dbOjects.js");
+const { Op } = require("sequelize");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("predict")
-        .setDescription(
-            "Predict the outcome of the current match and the first scorer"
-        )
+        .setDescription("Make or update a prediction for the current match")
         .addIntegerOption((option) =>
             option
-                .setName("home_team_score")
-                .setDescription("Home team score")
+                .setName("home-score")
+                .setDescription("Predicted home team score")
                 .setRequired(true)
         )
         .addIntegerOption((option) =>
             option
-                .setName("away_team_score")
-                .setDescription("Away team score")
+                .setName("away-score")
+                .setDescription("Predicted away team score")
                 .setRequired(true)
         )
         .addStringOption((option) =>
             option
-                .setName("first_scorer")
-                .setDescription("First goal scorer")
+                .setName("first-scorer")
+                .setDescription("Predicted first goalscorer")
                 .setRequired(true)
         ),
     async execute(interaction) {
         try {
-            // user inputs
-            const homeTeamScore =
-                interaction.options.getInteger("home_team_score");
-            const awayTeamScore =
-                interaction.options.getInteger("away_team_score");
-            const firstScorer = interaction.options.getString("first_scorer");
+            const homeScore = interaction.options.getInteger("home-score");
+            const awayScore = interaction.options.getInteger("away-score");
+            const firstScorer = interaction.options.getString("first-scorer");
 
-            // get current match to predict on
-            const currentMatch = await getCurrentMatch();
+            const currentMatch = await findCurrentMatch();
+            if (!currentMatch) {
+                const noMatchesEmbed = new EmbedBuilder()
+                    .setColor("#FF0000")
+                    .setTitle("🗓️ No Upcoming Matches")
+                    .setDescription(
+                        "There are no upcoming matches available to predict."
+                    );
+                return interaction.reply({ embeds: [noMatchesEmbed] });
+            }
 
-            // determine outcome based on scores
+            const currentTime = new Date();
+            const matchStartTime = new Date(
+                `${currentMatch.date} ${currentMatch.time}`
+            );
+
+            if (currentTime >= matchStartTime) {
+                const matchStartedEmbed = new EmbedBuilder()
+                    .setColor("#FF0000")
+                    .setTitle("🗓️ Match Has Started")
+                    .setDescription(
+                        "The match has already started. You can no longer change your prediction."
+                    );
+                return interaction.reply({ embeds: [matchStartedEmbed] });
+            }
+
+            let user = await User.findOne({
+                where: { user_id: interaction.user.id },
+            });
+
+            if (!user) {
+                user = await User.create({
+                    user_id: interaction.user.id,
+                    username: interaction.user.username,
+                });
+            }
+
+            const existingPrediction = await Prediction.findOne({
+                where: {
+                    user_id: interaction.user.id,
+                    match_id: currentMatch.id,
+                },
+            });
+
+            // determine outcome based on scores from sj perspective
             let result;
-            if (homeTeamScore === awayTeamScore) {
+            if (homeScore === awayScore) {
                 result = "Draw";
-            } else if (homeTeamScore > awayTeamScore) {
+            } else if (homeScore > awayScore) {
                 result =
                     currentMatch.homeTeam === "San Jose Earthquakes"
                         ? "Win"
@@ -53,7 +103,6 @@ module.exports = {
                         : "Win";
             }
 
-			// adding some indicators for fun
             let outcomeIndicator;
             switch (result) {
                 case "Draw":
@@ -67,39 +116,101 @@ module.exports = {
                     break;
             }
 
-            const embed = new EmbedBuilder()
-                .setColor("#0099ff")
-                .setTitle("Match Prediction")
-                .addFields(
-                    {
-                        name: "User",
-                        value: `${interaction.user}`,
-                        inline: true,
-                    },
-                    {
-                        name: "Match",
-                        value: `${currentMatch.homeTeam} ${homeTeamScore}:${awayTeamScore} ${currentMatch.awayTeam}`,
-                        inline: true,
-                    },
-                    { name: "\u200b", value: "\u200b", inline: true }, /// play around with this to get spacing and formatting right
-                    {
-                        name: "First Scorer",
-                        value: firstScorer,
-                        inline: true,
-                    },
-                    {
-                        name: "Outcome",
-                        value: `${result} ${outcomeIndicator}`,
-                        inline: true,
-                    }
-                );
+            if (existingPrediction) {
+                // update prediction
+                await existingPrediction.update({
+                    user_home_pred: homeScore,
+                    user_away_pred: awayScore,
+                    user_scorer: firstScorer,
+                });
 
-            await interaction.reply({ embeds: [embed] });
+                const updatedPredictionEmbed = new EmbedBuilder()
+                    .setColor("#0099ff")
+                    .setTitle("🏆⚽ Prediction Updated")
+                    .setDescription(
+                        `**User:** ${interaction.user}\n**Match:** ${currentMatch.home_team} ${homeScore}:${awayScore} ${currentMatch.away_team}\n**First Scorer:** ${firstScorer}\n**Outcome:** ${result} ${outcomeIndicator}`
+                    )
+                    .setThumbnail(interaction.user.displayAvatarURL())
+                    .setFooter({
+                        text: "You can change your prediction until the match starts.",
+                    });
+
+
+                return interaction.reply({ embeds: [updatedPredictionEmbed] });
+                
+            } else {
+                // create new prediction 
+                await Prediction.create({
+                    user_id: interaction.user.id,
+                    match_id: currentMatch.id,
+                    user_home_pred: homeScore,
+                    user_away_pred: awayScore,
+                    user_scorer: firstScorer,
+                    points_awarded: 0,
+                });
+
+                const newPredictionEmbed = new EmbedBuilder()
+                    .setColor("#0099ff")
+                    .setTitle("🏆⚽ Prediction Updated")
+                    .setDescription(
+                        `**User:** ${interaction.user}\n**Match:** ${currentMatch.home_team} ${homeScore}:${awayScore} ${currentMatch.away_team}\n**First Scorer:** ${firstScorer}\n**Outcome:** ${result} ${outcomeIndicator}`
+                    )
+                    .setThumbnail(interaction.user.displayAvatarURL())
+                    .setFooter({
+                        text: "You can change your prediction until the match starts.",
+                    });
+                return interaction.reply({ embeds: [newPredictionEmbed] });
+            }
         } catch (error) {
-            console.error("Error processing prediction:", error);
-            await interaction.reply(
-                "An error occurred while processing the prediction."
-            );
+            console.error("Error executing predict command:", error);
+            const errorEmbed = new EmbedBuilder()
+                .setColor("#FF0000")
+                .setTitle("🗓️ Error")
+                .setDescription(
+                    "An error occurred while processing your prediction."
+                );
+            await interaction.reply({ embeds: [errorEmbed] });
         }
     },
 };
+
+
+// might change the grace period here but for now will do 
+
+async function findCurrentMatch() {
+    const currentDate = new Date();
+
+    // Get the current date and time
+    const currentTime = currentDate.getTime();
+
+    // Fetch the upcoming match
+    const currentMatch = await Match.findOne({
+        where: {
+            date: { [Op.gt]: currentDate },
+        },
+        order: [
+            ["date", "ASC"],
+            ["time", "ASC"],
+        ],
+        limit: 1,
+    });
+
+    if (!currentMatch) {
+        return null; // No upcoming match found
+    }
+
+    // Calculate the start time of the match
+    const matchStartTime = new Date(
+        `${currentMatch.date} ${currentMatch.time}`
+    ).getTime();
+
+    // Define a grace period (e.g., 15 minutes) after which predictions are not allowed
+    const gracePeriodMillis = 15 * 60 * 1000; // 15 minutes in milliseconds
+
+    // Check if the current time is within the grace period before the match starts
+    if (matchStartTime - currentTime <= gracePeriodMillis) {
+        return null; // Match has already started or within the grace period
+    }
+
+    return currentMatch; // Return the upcoming match
+}
--- app/models/users.js
+++ app/models/users.js
@@ -1,49 +1,55 @@
+/*need to add to the following table
+a) aooearances (so how many times the user has predicted)
+b) rankings: current position, previous postion (already have highest and lowest position)
+c) last match prediction (what was the user last match prediction)
+*/
+
 module.exports = (sequelize, DataTypes) => {
 	return sequelize.define(
         "users",
         {
             user_id: {
-                type: DataTypes.INTEGER,
+                type: DataTypes.STRING,
                 primaryKey: true,
             },
             username: {
                 type: DataTypes.STRING,
-                allowNull: false,
+                //allowNull: false,
             },
             points: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                allowNull: false,
+                //allowNull: false,
             },
             highest_pos: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                allowNull: false,
+                //allowNull: false,
             },
             lowest_pos: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                allowNull: false,
+                //allowNull: false,
             },
             ppg: {
                 type: DataTypes.FLOAT,
                 defaultValue: 0,
-                allowNull: false,
+                //allowNull: false,
             },
             result: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                allowNull: false,
+                //allowNull: false,
             },
             first_scorer: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                allowNull: false,
+                //allowNull: false,
             },
             outcome: {
                 type: DataTypes.INTEGER,
                 defaultValue: 0,
-                allowNull: false,
+                //allowNull: false,
             },
         },
         {
