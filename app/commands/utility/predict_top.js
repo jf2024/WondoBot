e6aee87849ba7b36c4440ab5e8ac39c0c3d1e1e8
Jose Xavier Fuentes <josexfuentes27@gmail.com>
2024-04-26 22:42:04 -0700

    made predict-top better

--- app/commands/utility/predict_top.js
+++ app/commands/utility/predict_top.js
@@ -1,21 +1,48 @@
-const { SlashCommandBuilder, Collection } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const db = require("../../dbObjects");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("predict_top")
-    .setDescription("Users with top 10 highest prediction scores."),
-  async execute(interaction) {
-    const topPredictors = await db.User.findAll({
-      order: [["total_points", "DESC"]],
-      attributes: ["username"],
-      limit: 10,
-    });
-    if (!topPredictors) return interaction.reply("No top predictors found.");
-    return interaction.reply(
-      `Top 10 predictors: ${topPredictors
-        .map((user) => user.username)
-        .join(", ")}`
-    );
-  },
+    data: new SlashCommandBuilder()
+        .setName("predict_top")
+        .setDescription("Users with top 10 highest prediction scores."),
+    async execute(interaction) {
+        try {
+            const topPredictors = await db.User.findAll({
+                order: [["points", "DESC"]],
+                attributes: ["user_id", "username", "appearances", "points"],
+                limit: 10,
+            });
+
+            if (!topPredictors || topPredictors.length === 0) {
+                return interaction.reply("No top predictors found.");
+            }
+
+            const topPredictorsStrings = await Promise.all(
+                topPredictors.map(async (user, index) => {
+                    const discordUser = await interaction.client.users.fetch(
+                        user.user_id
+                    );
+                    const username = discordUser
+                        ? discordUser.toString()
+                        : user.username;
+                    return `${index + 1}) ${username} (🔥) - ${
+                        user.appearances
+                    } appearances: ${user.points} points`;
+                })
+            );
+
+            const embed = new EmbedBuilder()
+                .setColor("#0099ff")
+                .setTitle("🏆🏆 Top 10 Predictors")
+                .setDescription(topPredictorsStrings.join("\n"));
+
+            return interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error("Error executing predict_top command:", error);
+            return interaction.reply(
+                "An error occurred while fetching top predictors."
+            );
+        }
+    },
 };
+
